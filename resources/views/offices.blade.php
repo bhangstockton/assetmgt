dba0c62f91e34320c958d50ed2d56ecd6e32d7a5
Dennis Carlos <denniscarlos29@yahoo.com>
2016-04-14 16:31:28 +0800

    office blade update

--- resources/views/offices.blade.php
+++ resources/views/offices.blade.php
@@ -25,43 +25,34 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-12">
-			<!-- TABBOX: Title -->
+			<!-- TABBOX: Information -->
 			<div class="nav-tabs-custom">
 				<!-- Nav Tabs -->
 				<ul class="nav nav-tabs">
-					<li id='tn_' class="active"><a href="#tc_" data-toggle="tab">Tab 1</a></li>
-					<li id='tn_'><a href="#tc_" data-toggle="tab">Tab 2</a></li>
-					<li class="pull-right"><a href="#" class="text-muted"><i class="fa fa-institution"></i></a></li>
+					<li id='tn_list' class="active"><a href="#tc_list" data-toggle="tab">List</a></li>
+					<li id='tn_add_supplier'><a href="#tc_add_supplier" data-toggle="tab">Add Offices</a></li>
+					<li class="pull-right"><a href="#" class="text-muted"><i class="fa fa-medkit"></i></a></li>
 				</ul>
-				<!-- Tab Content -->
-				<div class="tab-content">
-					<div id='tc_' class="tab-pane active">
-						<b>How to use:</b>
-					
-						<p>Exactly like the original bootstrap tabs except you should use
-						the custom wrapper <code>.nav-tabs-custom</code> to achieve this style.</p>
-						A wonderful serenity has taken possession of my entire soul,
-						like these sweet mornings of spring which I enjoy with my whole heart.
-						I am alone, and feel the charm of existence in this spot,
-						which was created for the bliss of souls like mine. I am so happy,
-						my dear friend, so absorbed in the exquisite sense of mere tranquil existence,
-						that I neglect my talents. I should be incapable of drawing a single stroke
-						at the present moment; and yet I feel that I never was a greater artist than now.
-					</div>
-					<!-- /.tab-pane -->
-					<div id='tc_' class="tab-pane">
-						The European languages are members of the same family. Their separate existence is a myth.
-						For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
-						in their grammar, their pronunciation and their most common words. Everyone realizes why a
-						new common language would be desirable: one could refuse to pay expensive translators. To
-						achieve this, it would be necessary to have uniform grammar, pronunciation and more common
-						words. If several languages coalesce, the grammar of the resulting language is more simple
-						and regular than that of the individual languages.
-					</div>
-					<!-- /.tab-pane -->
-				</div>
 			</div>
-			<!-- /TABBOX -->
 		</div>
 	</div>
+	<!-- Tab Content -->
+	<div class="tab-content">
+					<div id='tc_list' class="tab-pane active">
+						<!-- TABTABLE: List of Suppliers -->
+						<div class="box-body table-responsive no-padding">
+						    <table class="table table-hover">
+						    	<!-- headers -->
+						        <tr>
+						            <td style="border-top: 0px !important;"><b>ID</b></td>
+						            <td style="border-top: 0px !important;"><b>Office Name</b></td>
+						            <td style="border-top: 0px !important;"><b>Address</b></td>
+						            <td class='text-center' style="width: 7%; border-top: 0px !important;"><b><i class="fa fa-bolt"></i> </b></td>
+						        </tr>
+						    </table>
+						</div>
+					</div>
+			</div>
+	</div>
+
 @stop
